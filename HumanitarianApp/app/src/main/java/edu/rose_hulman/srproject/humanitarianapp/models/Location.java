596cd8d5b401ef748eb4bae4b5e3d95cbee14e70
ChrisHavens <havenscs@rose-hulman.edu>
2015-10-02 16:02:17 -0400

    Updated location model

---
+++ HumanitarianApp/app/src/main/java/edu/rose_hulman/srproject/humanitarianapp/models/Location.java
@@ -0,0 +1,21 @@
+package edu.rose_hulman.srproject.humanitarianapp.models;
+
+public class Location {
+    private float lng;
+    private float lat;
+    private String name;
+
+    public Location(String name, int latHour, int latMinute, int latSecond, int lngHour, int lngMinute, int lngSecond) {
+        float hourMinAsFloat;
+        float hourMinAsInt;
+        hourMinAsInt = latMinute * Float.intBitsToFloat(60) + latSecond;
+        hourMinAsFloat = hourMinAsInt / 3600;
+        this.lat = latHour + hourMinAsFloat;
+
+        hourMinAsInt = lngMinute * Float.intBitsToFloat(60) + lngSecond;
+        hourMinAsFloat = hourMinAsInt / 3600;
+        this.lat = lngHour + hourMinAsFloat;
+    }
+
+
+}

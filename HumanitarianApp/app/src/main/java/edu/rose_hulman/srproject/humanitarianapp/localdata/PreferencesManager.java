b8c311c8cced170c68e4f420cb83757f793c2b4d
ChrisHavens <havenscs@rose-hulman.edu>
2016-01-31 17:05:09 -0500

    Added a slight bit more code, but about to do something possible dumb, so backing up now, just in case.

--- HumanitarianApp/app/src/main/java/edu/rose_hulman/srproject/humanitarianapp/localdata/PreferencesManager.java
+++ HumanitarianApp/app/src/main/java/edu/rose_hulman/srproject/humanitarianapp/localdata/PreferencesManager.java
@@ -11,4 +11,20 @@ public class PreferencesManager {
     public static void setPreferencesFile(SharedPreferences sharedPreferences){
         preferences = sharedPreferences;
     }
+
+    public static void setSyncType(boolean manualSync){
+
+    }
+
+    public static void setSyncDate(String date){
+
+    }
+
+    public static boolean getSyncType(){
+        return preferences.getBoolean("ManualSync", false);
+    }
+
+    public static String getSyncTime(String date){
+        return preferences.getString("SyncTime", "01-01-1970 00:00:00");
+    }
 }
